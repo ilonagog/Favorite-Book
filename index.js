31baed8900db017573e181c3112d496c705df276
ilona <gogiashvili.ilona@gmail.com>
2022-11-09 12:15:14 -0500

    Created new buttons, now i can add new book, create a new book's card

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+   // const dropDown = document.querySelector("#book-dropdown")
+
+    //dropDown.addEventListener('change', handleChange)
+
+
+const addBtn = document.querySelector("#new-book-btn")
+
+const bookFormContainer = document.querySelector(".container")
+
+let addBook = false
+
 
 document.addEventListener("DOMContentLoaded", () => {
-    getBooks()
+ //   getBooks()
     document.getElementById("book-header").addEventListener('click', getBooks)
-
-    const dropDown = document.querySelector("#book-dropdown")
-    
-    dropDown.addEventListener('change', handleChange)
 
     function getBooks() {
         const ul = document.getElementById("books-collection")
@@ -27,6 +34,48 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.querySelector("#books-collection").innerHTML = booksHTML.join('')
             })
     }
+
+bookFormContainer.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log(e.target.name)
+    const bookName = e.target.name.value
+    const bookAuthor = e.target.author.value
+    const bookImage = e.target.image.value
+
+
+fetch("http://localhost:3000/books", {
+method: "POST",
+headers:
+{
+    "Content-Type": "application/json",
+    Accept:"application/json"
+
+
+},
+body: JSON.stringify({
+    "name": "The Essential RUMI",
+    "author": "Coleman Barks",
+    "image": "https://cdn.shopify.com/s/files/1/0285/2821/4050/products/9780062046659_106a2888-c25e-4122-b125-6fb573f57076_424x.jpg?v=1667491936",
+    "likes": 92
+})
+
+})
+    .then(response=>response.json())
+    .then(newBook=>{
+        let newBookHTML =`
+        <div class="card">
+        <h2>${newBook.name}</h2>
+        <p> ${book.author}</p>
+        <img src=${book.image} class="book-card" />
+        <p>${book.likes} Likes </p>
+        <button data-id="${newBook.id}"class="Like-btn">Like ❤️ </button>
+        </div>
+        `
+        document.querySelector("#book-collection").innerHTML += newBookHTML
+   console.log(e.target.removeEventListener())
+    })
+
+})
 
     addEventListener('click', (e) => {
         if (e.target.className === "Like-btn") {
@@ -51,103 +100,115 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     // const dropDown = document.querySelector("#book-dropdown")
-    // dropDown.addEventListener('change', handleChage)
-    // function getBooks() {
-    //     fetch('http://localhost:3000/books')
-    //         .then(resp => resp.json())
-    //         .then(booksAuthors => {
+    // const dropDown = document.querySelector("#book-dropdown")
+    // dropDown.addEventListener('change', handleChange)
 
-    //             const booksArray = Object.keys(authors)
-    //             debugger
-    //             const booksLis = createLiElement(booksArray)
-    //             renderLis(booksLis)
-    //         })
-    // }
+    // selectElement.addEventListener('change', (event) => {
+    //     const result = document.querySelector('.result');
+    //     result.textContent = `You like ${event.target.value}`;
+    // });
+
+
+
+
+    /*
+    // let booksArray = [];
+    function getBooksAuthors() {
+        let booksArray = [];
+        fetch('http://localhost:3000/books')
+            .then(resp => resp.json())
+            .then(books => {
+
+                // booksArray.map((book) => {
+                let option = `<option>${books[0]['author']}</option>`
+                return option
+                //     return `
+                //    booksArray = books[0]['author']`
+                //  return getBooksAuthors.push(books.author)
+                //booksArray = books.map(books.author)
+                //Object.keys(books);
+                // debugger;
+
+                // const booksLis = createLiElement(booksArray)
+                // renderLis(booksOptions)
+            })
+    }
+
+    */
+    //  }
     // function createLiElement(bookArray) {
     //     return bookArray.map((book) => {
-    //         let li = `<li>${book}</li>`
-    //         return li
+    //         let option = `<option>${book}</option>`
+    //         return option
     //     })
     // }
-
-    // function renderLis(booksLis) {
+    // function renderOptions(booksOptions) {
     //     booksLis.forEach(element => {
     //         renderElement(element)
     //     })
     // }
     // function createLiElement(booksArray) {
     //     return booksArray.map((book) => {
-    //         let li = `<li>${book}</li>`
-    //         return li
+    //         let option = `<option>${book}</option>`
+    //         return option
     //     })
     // }
-
-
-    // function renderList(booksList) {
+    // function renderList(booksOptions) {
     //     booksList.forEach(element => {
     //         renderElement(element)
     //     })
-    // }
+    //  }
 
 
-
-
-    let bookAuthorsArray = []
-
-    function getAuthors() {
-        fetch(" http://localhost:3000/books")
-            .then(resp => resp.json())
-
-        //   bookAuthorsArray = Object.keys(books)
+    /*
+    
+        let bookAuthorsArray = []
+    
+        function getAuthors() {
+            fetch(" http://localhost:3000/books")
+                .then(resp => resp.json())
+    
+            //   bookAuthorsArray = Object.keys(books)
+        }
+    
+        function renderAllBooks(books) {
+    
+    
+        }
+    
+        function handleChange(event) {
+            const author = event.target.value
+    
+            //     //   console.log(bookAuthorsArray)
+            //     // const filteredBooksArray = bookAuthorsArray.filter(book => book.startsWith(author))
+            //     // const filteredBooks = bookAuthorsArray.filter(book => book.startsWith(author))
+            //     // const filteredBooksList = createLiElement(filteredBooks)
+            //     // ul.container.innerHTML = ""
+            //     // renderList(filteredBooksList)
+        }
+    
+        // getAuthors();
+    
+    
+    */
+    function handleChange(e) {
+        const author = e.target
     }
-
-    function renderAllBooks(books) {
-
-
+addBtn.addEventListener('click',()=>{
+    addBook=!addBook;
+    if(addBook){
+        bookFormContainer.style.display="block";
+    }else{
+        bookFormContainer.style.display= "none";
     }
+})
 
-    function handleChange(event) {
-        const author = event.target.value
-
-        //     //   console.log(bookAuthorsArray)
-        //     // const filteredBooksArray = bookAuthorsArray.filter(book => book.startsWith(author))
-        //     // const filteredBooks = bookAuthorsArray.filter(book => book.startsWith(author))
-        //     // const filteredBooksList = createLiElement(filteredBooks)
-        //     // ul.container.innerHTML = ""
-        //     // renderList(filteredBooksList)
-    }
-
-    // getAuthors();
-
-
-
-
-
-
+    
+    getBooks()
+    // getBooksAuthors()
 })
 
 
- //     function createDelete() {
-        //         let btn = document.createElement('button')
-        //         btn.addEventListener('click', deleteBook)
-        //         btn.textContent = 'Delete'
-        // ul.appendChild(btn)
-
-        //     }
-
-        // const deleteBtn = document.getElementById('delete-button');
-
-        //     function deleteBook(e) {
-        //         //delete -button.addEventListener('click', function (e) {
-        //         e.target.parentnode.remove()
-        //     }
-        // }
-        // deleteBook()
-
-
-        // let btn = document.createElement("button")
-        // btn.textContent = 'x'
-        // ul.appendChild(btn)
 
 
 
@@ -158,17 +219,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // let defaultOption = document.createElement('option');
     // defaultOption.text = "Choose Book by Author";
 
-    // dropdown.addEventListener(defaultOption);
-    // dropdown.selectedIndex = 0;
+  
 
-    // const url = "http://localhost:3000/books";
 
-    // const request = new XMLHttpRequest();
-    // request.open("GET", url, true);
-
-    // request.onload = function () {
-    //     if (request.status === 200) {
-    //         const data = JSON.parse(request.responseText);
 
     //         let option;
     //         for (let i = 0; i < data.length; i++) {
@@ -179,4 +232,4 @@ document.addEventListener("DOMContentLoaded", () => {
     //     } else {
 
     //     }
-    // }
+    // 
